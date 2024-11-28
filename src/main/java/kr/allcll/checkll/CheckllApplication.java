43411e8f0c7386af4bd5f4bc326a5c5f4a5bf9f6
Sumin <new.sumyang@gmail.com>
2024-11-28 13:23:27 +0900

    init: Initialize project

---
+++ src/main/java/kr/allcll/checkll/CheckllApplication.java
@@ -0,0 +1,13 @@
+package kr.allcll.checkll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckllApplication.class, args);
+	}
+
+}
