b762de27ebdea0a7ee008db52f8d4730a3f041c1
Juhwan Kim <13selfesteem91@naver.com>
2024-11-29 18:44:42 +0900

    feat: DataSource api 추가(인증 정보 설정, 시작과 중단) (#5)

--- src/main/java/kr/allcll/checkll/datasource/PastGradeDataSource.java
+++ src/main/java/kr/allcll/checkll/datasource/PastGradeDataSource.java
@@ -3,4 +3,10 @@ package kr.allcll.checkll.datasource;
 public interface PastGradeDataSource {
 
     PastGradeInfo getPastGradeInfo();
+
+    void setCredential(String credential);
+
+    void setUp();
+
+    void shutDown();
 }
