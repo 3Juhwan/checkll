fc97fee20114dfc040f9200d5a43d02849287106
3Juhwan <13selfesteem91@naver.com>
2024-11-29 23:56:50 +0900

    feat: 기이수 성적 객체에 싱글톤 저장소 생성

--- src/main/java/kr/allcll/checkll/datasource/PastGradeInfo.java
+++ src/main/java/kr/allcll/checkll/datasource/PastGradeInfo.java
@@ -1,10 +1,13 @@
 package kr.allcll.checkll.datasource;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 import lombok.Getter;
 
 @Getter
 public class PastGradeInfo {
+
+    private static PastGradeInfo instance;
 
     private final PastGrade pastGrade;
     private final LocalDateTime accessAt;
@@ -12,5 +15,16 @@ public class PastGradeInfo {
     public PastGradeInfo(PastGrade pastGrade, LocalDateTime accessAt) {
         this.pastGrade = pastGrade;
         this.accessAt = accessAt;
+    }
+
+    public static PastGradeInfo getInstance() {
+        if (instance == null) {
+            throw new NoSuchElementException("Past grade is not ready.");
+        }
+        return instance;
+    }
+
+    public static void update(PastGradeInfo pastGradeInfo) {
+        instance = pastGradeInfo;
     }
 }
