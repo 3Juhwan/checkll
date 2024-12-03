1272362e5e085e973a1e8b89a47d154664c91701
Sumin <new.sumyang@gmail.com>
2024-12-04 00:14:29 +0900

    refactor: 클래스 명 수정

--- src/main/java/kr/allcll/checkll/datasource/SubjectsRepository.java
+++ src/main/java/kr/allcll/checkll/datasource/SubjectsRepository.java
@@ -2,6 +2,6 @@ package kr.allcll.checkll.datasource;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PopUpRepository extends JpaRepository<Subjects, Long> {
+public interface SubjectsRepository extends JpaRepository<Subjects, Long> {
 
 }
