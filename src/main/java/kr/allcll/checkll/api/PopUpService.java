4354d82b6a125e24cecdcfd65b65acb46fb12a61
3Juhwan <13selfesteem91@naver.com>
2024-12-04 15:30:00 +0900

    feat: 여석 조회 스케줄러 추가

--- src/main/java/kr/allcll/checkll/api/PopUpService.java
+++ src/main/java/kr/allcll/checkll/api/PopUpService.java
@@ -1,18 +1,25 @@
 package kr.allcll.checkll.api;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import kr.allcll.checkll.datasource.PopUp;
 import kr.allcll.checkll.datasource.PopUpDataSource;
 import kr.allcll.checkll.datasource.PopUpInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class PopUpService {
 
+    private final RemainSeatsRepository remainSeatsRepository;
+
     private final PopUpDataSource popUpDataSource;
 
-    public PopUpService(PopUpDataSource popUpDataSource) {
+    public PopUpService(RemainSeatsRepository remainSeatsRepository, PopUpDataSource popUpDataSource) {
+        this.remainSeatsRepository = remainSeatsRepository;
         this.popUpDataSource = popUpDataSource;
     }
 
@@ -27,5 +34,22 @@ public class PopUpService {
             result.add(new PopUpResponse(courseName, department, remainCnt));
         }
         return result;
+    }
+
+    @Scheduled(fixedDelay = 1000 * 10)
+    public void saveRemainSeatsOfAllCourses() {
+        List<PopUpInfo> allCourseRegisterInfo = popUpDataSource.getCoursePopUp();
+        List<RemainSeats> remainSeats = new ArrayList<>();
+        for (PopUpInfo popUpInfo : allCourseRegisterInfo) {
+            PopUp popUp = popUpInfo.getPopUp();
+            remainSeats.add(new RemainSeats(
+                popUp.getDlMain().getFirst().getCuriNo(),
+                popUp.getDlMain().getFirst().getClassCode(),
+                popUp.getDlMain().getFirst().getDeptCd(),
+                popUp.getDlMain().getFirst().getRemainTotRcnt(),
+                LocalDateTime.now()
+            ));
+        }
+        remainSeatsRepository.saveAll(remainSeats);
     }
 }
